72526a72f15d33e08da4609ba078d5ba9ca39e9e
Yannickkerboull <yannick.kerboull@gmail.com>
2016-04-20 10:04:43 +0200

    script modif

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
+	compteur = 0;
+	var audio = new Audio("./songs/saut.wav");
+	var audio1 = new Audio("./songs/Mario.mp3");
+	var audio2 = new Audio("./songs/hurry-up.wav");
+	audio1.play();
+	
 	$(document).keydown(function(e){
 	
 		var p = $( ".perso" );
 		var offset = p.offset();
+		
+		var p1 = $( ".maps" );
+		var offset1 = p1.offset();
 		
 		switch (e.which){
 			case 37:
@@ -13,8 +22,13 @@ $(document).ready(function(){
 				}
 				break;
 			case 38:
+				//alert(compteur);
+				audio.play();
 				$(".perso").animate({ "top": "-=40px" }, 500);
-				$(".perso").animate({ "top": "+=40px" }, 500); break;
+				$(".perso").animate({ "top": "+=40px" }, 500); 
+				$( ".mus3" ).hide(1);
+				audio.stop();
+				break;
 			case 39:
 				$(".perso").css( "transform" , "scaleX(+1)" );
 				if (offset.left < 730)
@@ -23,7 +37,19 @@ $(document).ready(function(){
 				}
 				else
 				{
-					$(".maps").animate({ "background-position": "-=2px" }, 1);
+					//imgWidth = $('#hiddenImg').width();
+					compteur = compteur + 2;
+					if (compteur < 4190)
+					{
+						$(".maps").animate({ "background-position": "-=2px" }, 1);
+					}
+					else
+					{
+						audio1.pause();
+						audio2.play();
+						$(".perso").animate({ "top": "-=40px" }, 500);
+						$(".perso").animate({ "top": "+=40px" }, 500); 
+					}
 				}
 				break;
 		  }
